9e66aa478fc6a8895a35162ccfe24bdaff7ed0a4
Me-k-01 <mecaman01@gmail.com>
2021-10-04 16:27:59 +0200

    Expansion permutation

--- Des.java
+++ Des.java
@@ -10,18 +10,28 @@ class Des {
         62, 54, 46, 38, 30, 22, 14, 6,
         64, 56, 48, 40, 32, 24, 16, 8,
         
-        57, 49, 41, 33, 25, 17, 9, 1,
+        57, 49, 41, 33, 25, 17,  9, 1,
         59, 51, 43, 35, 27, 19, 11, 3,
         61, 53, 45, 37, 29, 21, 13, 5,
         63, 55, 47, 39, 31, 23, 15, 7
     }; 
 
-    public int[] compPerm = {
-        14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
-        23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
+    public int[] compPerm = { // Compression permutation 56 -> 48 bits
+        14, 17, 11, 24,  1,  5,  3, 28, 15,  6, 21, 10,
+        23, 19, 12,  4, 26,  8, 16,  7, 27, 20, 13,  2,
         41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
         44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32 
     };
+
+    public int[] expPerm = {  // Expansion permutation 32 -> 48 bits 
+        32,  1,  2,  3,  4,  5,  4,  5,
+         6,  7,  8,  9,  8,  9, 10, 11,
+        12, 13, 12, 13, 14, 15, 16, 17,
+        16, 17, 18, 19, 20, 21, 20, 21,
+        22, 23, 24, 25, 24, 25, 26, 27,
+        28, 29, 28, 29, 30, 31, 32,  1 
+    };
+
     private int size = 64;
 
     private Bloc[] blocs;
@@ -68,7 +78,8 @@ class Des {
 
     private int processK(Bloc G, Bloc D, int n) {
         Bloc key = generateKey() ;
-        
+        D.permut(this.expPerm); // Expansion permutation 32 -> 48
+
         return 0;
     }
     
@@ -87,7 +98,7 @@ class Des {
             // Deux partie sont recollées
             this.blocs[i] = Bloc.combine(splitedBloc);
             // Permutation inverse
-            this.blocs[i].invPermut(this.permInit);
+            //this.blocs[i].invPermut(this.permInit);
         }
         System.out.println(Arrays.deepToString(this.blocs));
         
